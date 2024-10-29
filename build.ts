e13e72347a760c660d065a472e1fe34c9a7bc285
Sergey Bricks <sb@smoothbricks.com>
2024-10-28 18:14:02 -0700

    Introduce Any* types to deal with user Events intersecting to `never`
    Flexible reducer configuration
    
    - Signia has not been updated in a while, turns out new development happens in @tldraw/state
    - Bundle it, avoiding extra dependencies that @tldraw/utils pulls in

--- build.ts
+++ build.ts
@@ -1,12 +1,19 @@
 import type { BuildConfig } from 'bun';
+import { $ } from 'bun';
 import dts from 'bun-plugin-dts';
 
 const defaultBuildConfig: BuildConfig = {
   entrypoints: ['./src/index.ts'],
   outdir: './dist',
+  packages: 'bundle',
+  splitting: true,
+  sourcemap: 'external',
+  external: ['react'],
 };
 
-await Promise.all([
+await $`rm -rf ./dist`;
+
+const [esm, cjs] = await Promise.all([
   Bun.build({
     ...defaultBuildConfig,
     plugins: [dts()],
@@ -19,3 +26,11 @@ await Promise.all([
     naming: '[dir]/[name].cjs',
   }),
 ]);
+
+if (!esm.success) {
+  console.log('ESM BUILD FAILED');
+  console.log(esm);
+} else if (!cjs.success) {
+  console.log('CJS BUILD FAILED');
+  console.log(cjs);
+}
