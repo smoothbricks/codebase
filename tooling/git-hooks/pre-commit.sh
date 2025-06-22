61dac054ad24975cb2276d290a66338050cf6423
Danny Wilson <git@danny.prime.vc>
2025-06-22 20:42:50 +0400

    feat: migrate git-format-staged to config file format
    Switch from multiple command-line invocations to a single YAML config file
    for git-format-staged v4. This simplifies the pre-commit hook and takes
    advantage of the new features:
    
    - Create git-format-staged.yml with formatters for ESLint, Biome, Prettier, and Alejandra
    - Update pre-commit.sh to use --config flag instead of multiple invocations
    - Update devenv to use smoothbricks/git-format-staged fork with submodule auto-exclusion
    - Remove manual submodule exclusion logic (now handled automatically)
    
    The new config format is cleaner, more maintainable, and leverages
    git-format-staged v4's improved gitignore-style pattern matching.

--- tooling/git-hooks/pre-commit.sh
+++ tooling/git-hooks/pre-commit.sh
@@ -5,27 +5,7 @@ TOOLING="$PWD/tooling"
 # Add hook scripts, bun installed tools and Devenv to PATH:
 export PATH="$TOOLING/git-hooks:$TOOLING:$PWD/node_modules/.bin:$TOOLING/direnv/.devenv/profile/bin:$PATH"
 
-# Make an exclusion list to ignore files in submodules
-SUBMODULE_EXCLUSIONS=$(git config --file .gitmodules --get-regexp '.path$' | awk '{ print "!" $2 "*" }' | tr '\n' ' ')
-
 # Make bash abort on errors and output commands
 set -e -o pipefail
 
-# Run `eslint --fix` for files not supported by Biome, except for hidden files:
-# - https://stackoverflow.com/questions/57947585/eslint-warning-file-ignored-by-default-use-a-negated-ignore-pattern
-git-format-staged --verbose -f \
-  "eslint-stdout '{}'" \
-   '!.*' '*.astro'
-
-# Format all files that Biome supports, excluding git submodules:
-git-format-staged --verbose -f \
-  "biome check --files-ignore-unknown=true --use-editorconfig=true '--stdin-file-path={}' --fix" $SUBMODULE_EXCLUSIONS \
-  '*.js' '*.ts' '*.jsx' '*.tsx' '*.json' '*.jsonc' '*.html' '*.css' '*.graphql'
-
-# Format other (Markdown) files with prettier:
-git-format-staged --verbose -f \
-  "prettier --ignore-unknown --stdin-filepath '{}'" '*' $SUBMODULE_EXCLUSIONS \
-  '!*.astro' '!*.js' '!*.ts' '!*.jsx' '!*.tsx' '!*.json' '!*.jsonc' '!*.html' '!*.css' '!*.graphql'
-
-# Nix
-git-format-staged --verbose -f "alejandra" '*.nix'
+git-format-staged
