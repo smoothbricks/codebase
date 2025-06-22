012ac70b59bb83b19905ca204ff02b072ba570b5
Danny Wilson <git@danny.prime.vc>
2025-06-22 23:24:17 +0400

    fix(tooling/direnv): run setup-environment.sh only once!

--- tooling/direnv/envrc.sh
+++ tooling/direnv/envrc.sh
@@ -15,9 +15,6 @@ fi
 source_url "https://raw.githubusercontent.com/cachix/devenv/95f329d49a8a5289d31e0982652f7058a189bfca/direnvrc" "sha256-d+8cBpDfDBj41inrADaJt+bDWhOktwslgoP5YiGJ1v0="
 use devenv
 
-# Source the shared environment setup
-source setup-environment.sh
-
 # Watch JS tooling changes
 watch_file bun.lock
 watch_file package.json
