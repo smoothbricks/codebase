4dd9fcbb6e1bf8ee77d6a4485455c49fdbbb8447
Danny Wilson <git@danny.prime.vc>
2026-08-20 19:39:23 +0400

    fix(tooling): directly load direnvrc from devenv
    Unblock shell entering when GitHub is down

--- tooling/direnv/envrc.sh
+++ tooling/direnv/envrc.sh
@@ -12,7 +12,7 @@ else
 fi
 
 # Devenv (Nix)
-source_url "https://raw.githubusercontent.com/cachix/devenv/95f329d49a8a5289d31e0982652f7058a189bfca/direnvrc" "sha256-d+8cBpDfDBj41inrADaJt+bDWhOktwslgoP5YiGJ1v0="
+eval "$(devenv direnvrc)"
 use devenv
 
 # Watch JS tooling changes
